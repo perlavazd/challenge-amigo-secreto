48a24f5f8d6b84b9edb650bd8eeaf92b609b0190
Perla <perlavazd@gmail.com>
2025-08-10 08:36:29 -0600

    Funciones para mostrar lista y amigo sorteado

--- app.js
+++ app.js
@@ -13,7 +13,39 @@ function agregarAmigo (event){
         console.log(amigos);
         document.getElementById('amigo').value = '';
     }
+    lista();
 }
+
+
+function lista (){
+
+    let listaGenerada = document.getElementById("listaAmigos");
+    listaGenerada.innerHTML = ""
+
+    for (let i=0; i<amigos.length; i++){
+        let elementoLi = document.createElement("li");
+        elementoLi.textContent = amigos[i];
+        listaGenerada.appendChild(elementoLi);
+    }
+
+}
+
+function sortearAmigo(event){
+    event.preventDefault();
+    if (amigos.length === 0){
+        alert("Por favor, inserte nombres antes de sortear");
+    }else{
+        let numeroSorteado= Math.floor(Math.random() * (amigos.length));
+        console.log(amigos[numeroSorteado]);
+
+        let listaResultado = document.getElementById("resultado");
+        listaResultado.innerHTML = ""
+        listaResultado.innerHTML = `<li>${amigos[numeroSorteado]}</li>`;
+
+    }
+}
+
+
 
 
 
